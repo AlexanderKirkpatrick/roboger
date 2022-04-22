6600b122b1b883855dd6f50082c4552ce09fab98
Alexander <knibz@protonmail.com>
2022-04-22 16:48:11 -0700

    add function numberSwap that passes test two

--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,17 @@ function inputArray (number) {
   return inputtedArray; 
   }
 
+  function numberSwap (element) {
+    if (element.includes(3)) {
+      return "Won't you be my neighbor?";
+    
+    } else if (element.includes(2)) {
+      return "Boop!";
+
+    } else if (element.includes(1)) {
+      return "Beep!";
+
+    } else {
+      return element;
+    }
+  }
